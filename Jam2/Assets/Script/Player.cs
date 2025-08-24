8138dc6bd72160b13d5fbc0eb9a6698f1730c0ac
RémiViard <remi.gabriel.viard@gmx.fr>
2025-08-24 18:13:08 +0200

    player + waterzone

--- Jam2/Assets/Script/Player.cs
+++ Jam2/Assets/Script/Player.cs
@@ -6,14 +6,24 @@ public class Player : MonoBehaviour
 {
     [SerializeField] InputActionAsset actions;
     [SerializeField] int baseMovementSpeed;
+    [SerializeField] int jumpForce;
+    Rigidbody2D rb;
     float movementSpeed;
     Vector2 movementInput = new Vector2();
     InputAction moveAction;
     [SerializeField] int dashCooldown;
     float dashTimer = 0;
+    PlayerState state = PlayerState.OnLand;
+    enum PlayerState
+    {
+        OnLand,
+        InWater,
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         moveAction = actions.FindAction("Move");
         actions.FindAction("Attack").performed += OnAttack;
         actions.FindAction("Dash").performed += OnDash;
@@ -28,7 +38,15 @@ public class Player : MonoBehaviour
         movementInput = moveAction.ReadValue<Vector2>();
         if(movementInput != Vector2.zero)
         {
-            transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed);
+            switch (state)
+            {
+                case PlayerState.OnLand:
+                    transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed);
+                    break;
+                case PlayerState.InWater:
+                    transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed);
+                    break;
+            }         
         }
         //Reduce dash Speed and Cooldown
         if (dashTimer > 0)
@@ -46,11 +64,53 @@ public class Player : MonoBehaviour
     }
     void OnDash(InputAction.CallbackContext callbackContext)
     {
-        if(dashTimer == 0)
+        switch (state)
         {
-            //movementSpeed *= 2;
-            dashTimer = dashCooldown;
+            case PlayerState.OnLand:
+                if(CheckGround())
+                {
+                    rb.linearVelocityY = jumpForce;
+                    //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                }
+                    
+                break;
+            case PlayerState.InWater:
+                if (dashTimer == 0)
+                {
+                    //movementSpeed *= 2;
+                    dashTimer = dashCooldown;
+                }
+                break;
         }
+        
     }
+    bool CheckGround()
+    {
+        float _distanceToTheGround = GetComponent<Collider2D>().bounds.extents.y;
 
+        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position + Vector3.down * _distanceToTheGround, Vector3.down,  + 0.3f);
+        Debug.Log(hit.Length);
+        
+        foreach (RaycastHit2D h in hit)
+        {
+            if (h.collider.gameObject.CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+    public void EnterWater()
+    {
+        state = PlayerState.InWater;  
+    }
+    public void ExitWater()
+    {
+        state = PlayerState.OnLand;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position + Vector3.down * 0.1f, transform.position + Vector3.down * 0.1f + Vector3.down * 0.01f);
+    }
 }
--- Jam2/Assets/Script/WaterZone.cs
+++ Jam2/Assets/Script/WaterZone.cs
@@ -4,6 +4,24 @@ public class WaterZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.EnterWater();
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.ExitWater();
+            }
+        }
     }
 }
