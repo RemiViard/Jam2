a3567ea72ef3cc498ec4d4a7af544ebe2d868457
Aurore-Be <aurore.berthet@hotmail.com>
2025-08-24 18:03:41 +0200

    Fishes are not working but dialogues yes
    :(

--- Jam2/Assets/Script/FishMvtBehavior.cs
+++ Jam2/Assets/Script/FishMvtBehavior.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class FishMvtBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform m_transform;
+
+    public float TimeBeforeChangingDirection = 3f;
+    public bool isAlive;
+    public float m_Speed;
+
+    private Rigidbody2D m_Rigidbody;
+
+    private float m_currentTimer;
     void Start()
     {
-        
+        isAlive = true;
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_currentTimer = TimeBeforeChangingDirection;
+        m_transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isAlive)
+        {
+            Move();
+            m_currentTimer -= Time.deltaTime;
+            if (m_currentTimer <= 0)
+            {
+                ChangeDirection();
+                m_currentTimer = TimeBeforeChangingDirection;
+            }
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        Vector3 rot = Random.insideUnitCircle;
+        m_transform.Rotate(rot.x, rot.y, rot.z);
+    }
+
+    private void Move()
+    {
+        m_Rigidbody.linearVelocity = m_transform.forward * m_Speed;
     }
 }
