911a5b02fe53713df1384c1514b8dd406e8c111e
XOXO O Oui mon Titou XOXO <remi.gabriel.viard@gmx.fr>
2025-08-27 21:57:21 +0200

    hurt bix/ hitbox +Bob turn +terrain polygon collider 2d
    Toto prend moi

--- Jam2/Assets/Script/Hitbox.cs
+++ Jam2/Assets/Script/Hitbox.cs
@@ -1,16 +1,23 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Hitbox : MonoBehaviour
 {
+    Collider2D collider2D;
+    List<GameObject> touch = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<Collider2D>();
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!touch.Contains(collision.gameObject))
+        {
+            touch.Add(collision.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
