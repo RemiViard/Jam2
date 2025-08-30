7d1620b4e238ca7e35c95db11e8c1e7029f16364
SanguinisFrater <remi.gabriel.viard@gmx.fr>
2025-08-30 23:56:16 +0200

    caustics mat

--- Jam2/Assets/Script/Fog_PostProcess.cs
+++ Jam2/Assets/Script/Fog_PostProcess.cs
@@ -1,15 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Fog_PostProcess : MonoBehaviour
 {
     [SerializeField] Player _player;
+    [SerializeField] Terrain terrain;
+    [SerializeField] List<Material> sandMat = new List<Material>();
     float watersurface = -2.66f;
     float endSurface = -5f;
     Vector2 fogDensityMinMax = new Vector2(0f, 0.021f);
+    private void Start()
+    {
+        terrain.materialTemplate = sandMat[1];
+        //sandMaterial.SetPropertyLock("CausticsIsActive", true);
+    }
     private void Update()
     {
         if (_player.transform.position.y < watersurface)
         {
+            if(terrain.materialTemplate != sandMat[1])
+                terrain.materialTemplate = sandMat[1];
             if (!RenderSettings.fog)
                 RenderSettings.fog = true;
             float t = Mathf.InverseLerp(watersurface, endSurface, _player.transform.position.y);
@@ -21,6 +32,8 @@ public class Fog_PostProcess : MonoBehaviour
         {
             if (RenderSettings.fog)
                 RenderSettings.fog = false;
+            if (terrain.materialTemplate != sandMat[0])
+                terrain.materialTemplate = sandMat[0];
         }
 
     }
